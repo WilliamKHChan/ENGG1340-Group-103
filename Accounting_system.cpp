632f6585e98e956dc076c68554f2b914650ff3cc
WilliamKHChan <47054182+WilliamKHChan@users.noreply.github.com>
2019-03-15 21:32:19 +0800

    Add files via upload

--- Accounting_system.cpp
+++ Accounting_system.cpp
@@ -1,11 +1,12 @@
 #include "data_manipulation.h"
 using namespace std;
 int main() {
-	const string info_filename="Information.txt",account_filename="Account.txt",record_filename="Record.txt",budget_filename="Budget.txt";
+	//const string info_filename="Information.txt",account_filename="Account.txt",record_filename="Record.txt",budget_filename="Budget.txt";
 	User user;
+	vector<string> filename={"Information.txt","Account.txt","Record.txt","Budget.txt"};
 	user.username="JasonChan";
 	user.password="jc1234";
-	if(LoadAll(user,info_filename,account_filename,record_filename,budget_filename)) {
+	if(LoadAll(user,filename)) {
 		cout<<"Name: "<<user.name<<endl<<"\nAccounts:\n";
 		for(auto i:user.account) {
 			cout<<i.name<<" "<<i.amount<<endl;
@@ -19,5 +20,6 @@ int main() {
 			cout<<i.start<<" "<<i.end<<" "<<i.type<<" "<<i.amount<<endl;
 		}
 	}
+	UpdateAll(user,filename);
 	return 0;
 }
